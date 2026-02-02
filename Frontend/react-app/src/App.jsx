602795fe8d7e55aee78d78583c56e43a438b808e
SagiBeno <benosagi321@gmail.com>
2026-02-02 18:44:45 +0100

    Chatbot implementation

--- Frontend/react-app/src/App.jsx
+++ Frontend/react-app/src/App.jsx
@@ -1,12 +1,12 @@
-/** App.jsx - TODO - import dependant modules */
+/** App.jsx - import dependant modules */
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
-import { Flex, Box, Text, TextField, IconButton, Spinner, ScrollArea, Strong } from "@radix-ui/themes";
-import { PaperPlaneIcon, FaceIcon } from "@radix-ui/react-icons";
-//import './App.css';
+import { Flex, Box, Text, TextField, IconButton, Spinner, ScrollArea, Strong, Card } from "@radix-ui/themes";
+import { PaperPlaneIcon, DesktopIcon, PersonIcon } from "@radix-ui/react-icons";
+import './App.css';
 import { Mistral } from '@mistralai/mistralai';
 
-/* TODO - obtain Mistral API key */
+/* obtain Mistral API key */
 export default class App extends React.Component {
   state = {
     isLoading: false,
@@ -17,6 +17,24 @@ export default class App extends React.Component {
       messages: [],
     },
     question: '',
+    userStyle: {
+      backgroundColor: 'orange',
+      padding: '5px',
+      width: '70%',
+      margin: '5px'
+    },
+    assistantStyle: {
+      padding: '5px',
+      width: '70%',
+      margin: '5px'
+    }
+  };
+
+  messagesEndRef = React.createRef();
+  scrollToBottom = () => {
+    this.messagesEndRef.current?.scrollIntoView({
+      behavior: 'smooth'
+    });
   };
 
   componentDidMount() {
@@ -27,42 +45,136 @@ export default class App extends React.Component {
     // 'read https://docs.mistral.ai/api/endpoint/chat'
     console.log('mistralClient', this.mistralClient)
 
-    this.setState({ mistralAiApiKey, mistralClient: this.mistralClient })
+    this.setState({ mistralAiApiKey, mistralClient: this.mistralClient });
+
+    fetch('http://localhost:3333/messages')
+      .then(res => res.json())
+      .then(data => {
+        console.log('GET /messages response', data);
+
+        if (Array.isArray(data.results)) {
+          this.setState(prev => ({
+            conversation: {
+              ...prev.conversation,
+              messages: data.results.map(m => ({
+                role: m.role,
+                message_content: m.message_content
+              }))
+            }
+          }),
+          this.scrollToBottom
+        );
+      }})
+      .catch(err => {
+        console.warn('GET /messages error', err);
+      });
   }
 
   sendQuestion = async () => {
-    //console.log('sendQuestion state', this.state);
-    const question = 'TODO'
-    //console.log('sendQuestion question', question);
+    console.log('sendQuestion state', this.state);
 
-    const requestBodyObj = { ...this.state.conversation };
-    requestBodyObj.messages.push({ role: 'user', content: 'TODO' })
-    //console.log('requestBodyObj', requestBodyObj)
+    const question = this.state.question;
 
-    // TODO - callout to POST /messages
+    if (question.length === 0) return;
 
-    const mistralClient = this.state.mistralClient;
-    // TODO - show spinner here
-    let chatResponse = 'TODO read https://docs.mistral.ai/api/endpoint/chat '
-    // TODO - stop spinner here
-    //console.log('chatResponse', chatResponse)
+    console.log('sendQuestion question', question);
 
-    // TODO - Update conversation with response
-    let updatedMessages = 'TODO'
-    //console.log('updatedMessages', updatedMessages)
+    this.setState({ question: '', isLoading: true });
 
-    // TODO - callout to POST /messages
+    try {
+      const requestBodyObj = { ...this.state.conversation };
+      console.log('requestBodyObj', requestBodyObj);
 
-    // TODO - setState
+      this.setState(prev => ({
+        conversation: {
+          ...prev.conversation,
+          messages: [
+            ...prev.conversation.messages,
+            { role: 'user', message_content: question }
+          ]
+        }}),
+        this.scrollToBottom
+      );
+
+      // callout to POST /messages (USER)
+      try {
+        const resJSON = await fetch('http://localhost:3333/messages', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ role: 'user', message_content: question })
+        });
+
+        const res = await resJSON.json();
+        console.log('POST: ' + res);
+      } catch (e) {
+        console.warn(e);
+      }
+
+      const mistralClient = this.state.mistralClient;
+      console.log('mistralClient in sendQuestion', mistralClient);
+
+      if (!mistralClient) {
+        console.warn('mistralClient is null - componentDidMount még nem futott le?');
+        return;
+      }
+
+      let chatResponse = await mistralClient.chat.complete({
+        model: this.state.conversation.model,
+        messages: [
+          {
+            content: question,
+            role: 'user',
+          },
+        ],
+      });
+
+      console.log('chatResponse', chatResponse);
+
+      // Update conversation with response
+      let updatedMessages = {
+        role: chatResponse.choices[0].message.role,
+        message_content: chatResponse.choices[0].message.content
+      };
+      console.log('updatedMessages', updatedMessages);
+
+      this.setState(prev => ({
+        conversation: {
+          ...prev.conversation,
+          messages: [
+            ...prev.conversation.messages,
+            updatedMessages
+          ]
+        }}),
+        this.scrollToBottom
+      );
+
+      // callout to POST /messages (ASSISTANT)
+      try {
+        const resJSON2 = await fetch('http://localhost:3333/messages', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(updatedMessages)
+        });
+
+        const res2 = await resJSON2.json();
+        console.log('POST: ' + res2);
+      } catch (e) {
+        console.warn(e);
+      }
+
+    } catch (err) {
+      console.error('sendQuestion hiba:', err);
+    } finally {
+      this.setState({ isLoading: false });
+    }
   }
-
   handleEnter = e => {
-    if (e.key == 'Enter') this.sendQuestion();
+    if (e.key == 'Enter' && this.state.question.length > 0) this.sendQuestion();
   }
 
   render() {
     return (
-      <Flex direction="column" height="100vh" p="3" gap="3" style={{ margin: '10px' }}>
+      <Flex direction="column" height="90vh" p="3" gap="3" style={{ margin: '10px' }} width='90vw'>
         {/* Header */}
         <Box>
 
@@ -85,6 +197,7 @@ export default class App extends React.Component {
             backgroundColor: "#fafafa"
           }}
         >
+
           <Box p="2" style={{ padding: '2px' }}>
             {this.state.conversation.messages.length === 0 ? (
               <Text color="gray" style={{ userSelect: "none", opacity: '0.5' }}>No messages yet…</Text>
@@ -92,17 +205,25 @@ export default class App extends React.Component {
               this.state.conversation.messages.map((msg, i) => {
 
                 // Remove <think>...</think>
-                const cleaned = msg.content.replace(/<think>[\s\S]*?<\/think>/, '').trim();
+                const cleaned = msg.message_content.replace(/<think>[\s\S]*?<\/think>/, '').trim();
                 return (
-                  <div key={i}>
-                    <h5><FaceIcon /> {msg.role}</h5>
-                    <ReactMarkdown>{cleaned}</ReactMarkdown>
-                  </div>
+
+                  <Flex justify={msg.role === 'user' ? "end" : "start"} key={i}>
+                    <Card
+                      style={msg.role === 'user' ? this.state.userStyle : this.state.assistantStyle}
+                    >
+                      <h5>{msg.role === 'user' ? <PersonIcon /> : <DesktopIcon />} {msg.role}</h5>
+                      <ReactMarkdown>{cleaned}</ReactMarkdown>
+                    </Card>
+                  </Flex>
+
                 );
 
               })
             )}
           </Box>
+
+          <div ref={this.messagesEndRef} />
         </div>
 
         {/* Input bar or Spinner */}
@@ -111,25 +232,26 @@ export default class App extends React.Component {
           direction='row'
           justify='between'
           align='center'
-          style={{ minWidth: '90vw', minHeight: '40px' }}
+          style={{ minWidth: '100%', minHeight: '40px' }}
         >
-            <input
-              style={{width: '95%', minHeight: '100%', borderRadius: '20px', padding: '0 5px', marginRight: '5px'}}
-              type="text"
-              id="inputQuestion"
-              name="inputQuestion"
-              placeholder="Ask…"
-              onChange={e => this.setState({ question: e.target.value })} 
-            />
-              {
-                this.state.isLoading 
-                  ? 
-                    <Spinner size='2' /> 
-                  :
-                    <IconButton onClick={this.sendQuestion} id='btnSend' name='btnSend' aria-label='btnSend'>
-                      <PaperPlaneIcon />
-                    </IconButton>
-              }
+          <input
+            style={{ width: '95%', minHeight: '100%', borderRadius: '20px', padding: '0 5px', marginRight: '5px' }}
+            type="text"
+            id="inputQuestion"
+            name="inputQuestion"
+            placeholder="Ask…"
+            onChange={e => this.setState({ question: e.target.value })}
+            value={this.state.question}
+          />
+          {
+            this.state.isLoading
+              ?
+              <Spinner size='2' />
+              :
+              <IconButton onClick={this.sendQuestion} id='btnSend' name='btnSend' aria-label='btnSend'>
+                <PaperPlaneIcon />
+              </IconButton>
+          }
 
         </Flex>
       </Flex>
